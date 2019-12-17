04af74cbb50668cfd77119adf0e7b5a8a246e27c
RenZhanFeng <947625259@qq.com>
2019-12-17 17:44:57 +0800

    轮播组件，半成品

---
+++ src/common/js/dom.js
@@ -0,0 +1,15 @@
+/给dom添加类名/
+export function addClass(el, className) {
+    if (hasClass(el, className)) {
+        return
+    } else {
+        let newClass = el.className.split(' ')
+        newClass.push(className)
+        el.className = newClass.join(' ')
+    }
+}
+/判断该dom是否有这个类名/
+export function hasClass(el, className) {
+    let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+    return reg.test(el.className)
+}
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,14 +1,69 @@
 <template>
-  <div class="recommend">recommend</div>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+        <slider>
+          <div v-for="item of ImageUrl" :key="item.id">
+            <img :src="item.imgUrl" alt="img" />
+          </div>
+        </slider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Slider from "../../base/slider/slider";
+
 export default {
-  name: "recommend"
+  name: "recommend",
+  data() {
+    return {
+      ImageUrl: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg"
+        }
+      ]
+    };
+  },
+  components: {
+    Slider
+  }
 };
 </script>
 
 <style scoped lang="stylus">
 @import '../../common/stylus/variable';
-@import '../../common/stylus/mixin';
+
+.recommend
+  position fixed
+  width 100%
+  top 88px
+  bottom 0
+  .recommend-content
+    height 100%
+    overflow hidden
+    .slider-wrapper
+      position relative
+      width 100%
+      overflow hidden
+    
+    
 </style>
