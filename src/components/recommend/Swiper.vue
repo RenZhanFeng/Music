ac1bcea8f9cdd17cd2e81e7d489180dc58800c4e
RenZhanFeng <947625259@qq.com>
2019-12-18 15:43:23 +0800

    把轮播图拆分到一个单独的组件

---
+++ src/components/recommend/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in list" :key="item.id">
+        <img class="siwper-img" :src="item.imgUrl" alt="img" />
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>-->
+      <!-- <div class="swiper-scrollbar" slot="scrollbar"></div> -->
+    </swiper>
+  </div>
+</template>
+
+<script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+export default {
+  name: "Swiper",
+  props:{
+      list:Array
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+        effect: "fade",
+        speed: 2000,
+        autoplay: {
+          delay: 2000
+        }
+      }
+      
+    };
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+@import '../../common/stylus/variable';
+
+.siwper-img {
+  width: 100%;
+}
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,40 +2,19 @@
   <div class="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper">
-        <swiper :options="swiperOption">
-          <!-- slides -->
-          <swiper-slide v-for="item in ImageUrl" :key="item.id">
-            <img class="siwper-img" :src="item.imgUrl" alt="img" />
-          </swiper-slide>
-
-          <!-- Optional controls -->
-          <div class="swiper-pagination" slot="pagination"></div>
-          <!-- <div class="swiper-button-prev" slot="button-prev"></div>
-          <div class="swiper-button-next" slot="button-next"></div>-->
-          <!-- <div class="swiper-scrollbar" slot="scrollbar"></div> -->
-        </swiper>
+        <swiper :list="ImageUrl"></swiper>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import "swiper/dist/css/swiper.css";
-import { swiper, swiperSlide } from "vue-awesome-swiper";
+import Swiper from "./Swiper";
 
 export default {
   name: "recommend",
   data() {
     return {
-      swiperOption: {
-        pagination: ".swiper-pagination",
-        loop: true,
-        effect: "fade",
-        speed:2000,
-        autoplay: {
-          delay: 2000
-        }
-      },
       ImageUrl: [
         {
           id: "0001",
@@ -62,13 +41,11 @@ export default {
           imgUrl:
             "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911001.jpg"
         }
-        
       ]
     };
   },
   components: {
-    swiper,
-    swiperSlide
+    Swiper
   }
 };
 </script>
