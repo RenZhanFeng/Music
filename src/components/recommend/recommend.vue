db7272371491b1c447bcffce0355537d25ba979e
RenZhanFeng <947625259@qq.com>
2019-12-18 15:20:31 +0800

    使用vue-Awesome-Swiper插件完成推荐页轮播图

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,49 +2,73 @@
   <div class="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper">
-        <slider>
-          <div v-for="item of ImageUrl" :key="item.id">
-            <img :src="item.imgUrl" alt="img" />
-          </div>
-        </slider>
+        <swiper :options="swiperOption">
+          <!-- slides -->
+          <swiper-slide v-for="item in ImageUrl" :key="item.id">
+            <img class="siwper-img" :src="item.imgUrl" alt="img" />
+          </swiper-slide>
+
+          <!-- Optional controls -->
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>-->
+          <!-- <div class="swiper-scrollbar" slot="scrollbar"></div> -->
+        </swiper>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Slider from "../../base/slider/slider";
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 export default {
   name: "recommend",
   data() {
     return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true,
+        effect: "fade",
+        speed:2000,
+        autoplay: {
+          delay: 2000
+        }
+      },
       ImageUrl: [
         {
           id: "0001",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
+            "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911779.jpg"
         },
         {
           id: "0002",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
+            "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911780.jpg"
         },
         {
           id: "0003",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
+            "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911570.jpeg"
         },
         {
           id: "0004",
           imgUrl:
-            "http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg"
+            "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911563.jpg"
+        },
+        {
+          id: "0005",
+          imgUrl:
+            "http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1911001.jpg"
         }
+        
       ]
     };
   },
   components: {
-    Slider
+    swiper,
+    swiperSlide
   }
 };
 </script>
@@ -52,18 +76,25 @@ export default {
 <style scoped lang="stylus">
 @import '../../common/stylus/variable';
 
-.recommend
-  position fixed
-  width 100%
-  top 88px
-  bottom 0
-  .recommend-content
-    height 100%
-    overflow hidden
-    .slider-wrapper
-      position relative
-      width 100%
-      overflow hidden
-    
-    
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+
+.recommend-content {
+  height: 100%;
+  overflow: hidden;
+}
+
+.slider-wrapper {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+
+.siwper-img {
+  width: 100%;
+}
 </style>
